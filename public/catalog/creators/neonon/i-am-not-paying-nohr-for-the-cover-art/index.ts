3cbb39ce41abb7d886a6e4bb2ecf6580fcb78b6a
rpdeshaies <rpdeshaies@gmail.com>
2023-04-25 09:21:03 -0400

    feat: progress

---
+++ public/catalog/creators/neonon/i-am-not-paying-nohr-for-the-cover-art/index.ts
@@ -0,0 +1,7 @@
+import { IProjectData } from "public/catalog/loader";
+
+export function getData(): IProjectData {
+  return {
+    name: "I am not paying Nohr for the cover art",
+  };
+}
---
+++ public/catalog/creators/neonon/index.ts
@@ -0,0 +1,11 @@
+import { ICreatorData } from "public/catalog/loader";
+
+export function getData(): ICreatorData {
+  return {
+    name: "NeonoN",
+    links: {
+      Twitter: "https://twitter.com/4illeen",
+      Itch: "https://neonon.itch.io/",
+    },
+  };
+}
