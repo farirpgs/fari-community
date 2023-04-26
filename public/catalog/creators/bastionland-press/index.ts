4fb659b754400d6cc5e86951fde5e1744cc17fce
rpdeshaies <rpdeshaies@gmail.com>
2023-04-26 09:38:08 -0400

    fix: bastion land description

--- public/catalog/creators/bastionland-press/index.ts
+++ public/catalog/creators/bastionland-press/index.ts
@@ -3,6 +3,7 @@ import { ICreatorData } from "public/catalog/loader";
 export function getData(): ICreatorData {
   return {
     name: "Bastionland Press",
+    description: "Tabletop roleplaying games light on rules, heavy on flavour.",
     links: {
       Website: "https://www.bastionland.com",
       "Itch.": "https://chrismcdee.itch.io/",
