1625929bb40fa6aad7bf0c8e0963d75f441f1cd2
rpdeshaies <rpdeshaies@gmail.com>
2023-09-05 17:55:20 -0400

    fix: gist colors

--- public/catalog/creators/zotiquest-games/gist/index.ts
+++ public/catalog/creators/zotiquest-games/gist/index.ts
@@ -3,8 +3,7 @@ import { IProjectData } from "public/catalog/loader";
 export default function getData(): IProjectData {
   return {
     name: "GIST!",
-    description:
-      "Rules-light generic 2d6 RPG.",
+    description: "Rules-light generic 2d6 RPG.",
     license: "CC BY SA 4.0",
     links: {
       "itch.io": "https://zeruhur.itch.io/gist",
@@ -15,12 +14,9 @@ export default function getData(): IProjectData {
     @import url('https://fonts.googleapis.com/css?family=Noto Sans:400,400i,700,700i');
     article p {
         font-family: 'Noto Sans', serif !important;
-        font-size: 1em !important;
-        color: black !important;
     }
     article h1, h2, h3, h4, h5, h6 {
-        color: black !important;
     }
     `,
-};
+  };
 }
