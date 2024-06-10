2af066f0bd249fdec27856cfec508b7fc04381ab
rpdeshaies <rpdeshaies@gmail.com>
2024-06-10 12:07:05 -0400

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  prefetch: true,
+  integrations: [tailwind(), react()],
 });
