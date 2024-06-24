38c91a5a38c8ab724d012bd5b70709f6df1541b4
wipeoutbco <64807029+wipeoutbco@users.noreply.github.com>
2024-06-24 14:10:16 -0400

    Create index.ts
    adding loader index

---
+++ public/catalog/creators/when-suddenly-games/index.ts
@@ -0,0 +1,12 @@
+import { ICreatorData } from "public/catalog/loader";
+
+export default function getData(): ICreatorData {
+  return {
+    name: "When Suddenly! Games",
+    links: {
+      twitter: "https://twitter.com/whensuddengames",
+      tiktok: "https://tiktok.com/whensuddenlygames",
+      facebook: "https://facebook.com/whensuddenlygames",
+    },
+  };
+}
