e4cacb578f206325c85ed0253af3cf7d0c0e2fff
wipeoutbco <64807029+wipeoutbco@users.noreply.github.com>
2024-06-24 14:12:15 -0400

    Create index.ts
    adding index.ts

---
+++ public/catalog/creators/when-suddenly-games/imposition-srd/index.ts
@@ -0,0 +1,13 @@
+import { IProjectData } from "public/catalog/loader";
+
+export default function getData(): IProjectData {
+  return {
+    name: "Imposition SRD",
+    license: "CC BY-SA 4.0",
+    links: {
+      twitter: "https://twitter.com/whensuddengames",
+      Website: "https://www.whensuddenly.games",
+      itch: "https://whensuddenlygames.itch.io",
+    },
+  };
+}
