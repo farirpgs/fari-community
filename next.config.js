d347b7d1b72f4d59dba46869787c0a939639d770
rpdeshaies <rpdeshaies@gmail.com>
2024-11-13 08:13:10 -0500

    feat: keeper redirects (#89)

--- next.config.js
+++ next.config.js
@@ -1,8 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     unoptimized: true,
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "https://keeper.farirpgs.com",
+        permanent: false,
+      },
+      {
+        source: "/browse",
+        destination: "https://keeper.farirpgs.com",
+        permanent: false,
+      },
+      {
+        source: "/creators/:creatorSlug",
+        destination: "https://keeper.farirpgs.com/creators/:creatorSlug",
+        permanent: false,
+      },
+      {
+        source: "/creators/:creatorSlug/projects/:projectSlug",
+        destination:
+          "https://keeper.farirpgs.com/resources/:creatorSlug/:projectSlug",
+        permanent: false,
+      },
+      {
+        source: "/creators/:creatorSlug/projects/:projectSlug/:pageSlug",
+        destination:
+          "https://keeper.farirpgs.com/resources/:creatorSlug/:projectSlug/:pageSlug",
+        permanent: false,
+      },
+    ];
   },
 };
 
